b405d80f7aa5c08a24ae2306500288bb549e2b40
Sir Felix Delazar <63253390+SirFelixDelazar@users.noreply.github.com>
2020-06-19 16:34:58 +0500

    Clean the code

--- js/main.js
+++ js/main.js
@@ -1,299 +1,171 @@
-var g_program = [];
-var g_ram = [];
-var g_register = 0;
+var g_program = []
+var g_ram = []
+var g_register = 0
 
 function compile() {
-    const input = document.getElementById('in').value.replace(/(\n|\t|\r|\v)/g, ' ').replace(/\s\s+/g, ' ').trim();
+    const input = document.getElementById('in').value.replace(/(\n|\t|\r|\v)/g, ' ').replace(/\s\s+/g, ' ').trim()
 
     if(input == "") {
-        console.error('Program is empty');
-        return false;
+        console.error('the program is empty')
+        alert('the program is empty')
+        return false
     }
 
-    const program = input.split(' ');
-    //const strict = document.getElementById('strict-mode').checked;
+    const program = input.split(' ')
 
-    if (!checkSyntax(program, true)) return;
+    if (!checkSyntax(program)) return
 
     for(let i = 0; i < 255; i++) {
-        g_ram[i] = 0;
+        g_ram[i] = 0
     }
 
     for(let i = 0; i < 255; i++) {
-        g_program[i] = 0;
+        g_program[i] = 0
     }
+
+    g_register = 0;
 
     program.forEach((element, id) => {
-        setProgram(element, id);
-    });
+        g_program[id] = element;
+    })
     
-    run();
+    run()
 }
 
 function run() {
-    let statement = 0;
+    let statement = 0
     while(true) {
-        if(g_program[statement] == 0) break;
+        if(g_program[statement] == 0) break
+        
         if(g_program[statement].match(/^G/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            g_register = g_ram[num];
-            statement++;
+            g_register = g_ram[getArg(g_program[statement])]
+            statement++
+            continue
         }
+
         if(g_program[statement].match(/^O/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            g_ram[num] = g_register;
-            statement++;
+            g_ram[getArg(g_program[statement])] = g_register
+            statement++
+            continue
         }
+
         if(g_program[statement].match(/^R$/)) {
-            let read = 0;
-            g_register = (read = parseInt(prompt('input'))) > 255 && read < 0 ? 0 : read;
-            statement++;
-            continue;
+            let read = 0
+            g_register = (read = parseInt(prompt('input'))) > 255 && read < 0 ? 0 : read
+            statement++
+            continue
         }
+
         if(g_program[statement].match(/^B/)) {
-            if (g_register == 0) {
-                let num;
-                if(g_program[statement].length == 2) {
-                    num = parseInt(g_program[statement].charAt(1));
-                }
-                else if(g_program[statement].length == 3) {
-                    num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-                }
-                else {
-                    num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-                }
-                statement = num;
-                continue;
-            } else {
-                statement++;
-                continue;
-            }
+            if (g_register == 0)
+                statement = getArg(g_program[statement])
+             else 
+                statement++
+            continue
         }
+
         if(g_program[statement].match(/^I/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            g_register += num;
-            g_register %= 256;
-            statement++;
-            continue;
+            g_register += getArg(g_program[statement])
+            g_register %= 256
+            statement++
+            continue
         }
+
         if(g_program[statement].match(/^T$/)) {
-            alert(g_register);
-            statement++;
-            continue;
+            alert(g_register)
+            statement++
+            continue
         }
+
         if(g_program[statement].match(/^S/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            g_register = num;
-            statement++;
-            continue;
+            g_register = getArg(g_program[statement])
+            statement++
+            continue
         }
+
         if(g_program[statement].match(/^A/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            g_register += g_ram[num];
-            g_register %= 256;
-            statement++;
-            continue;
+            g_register += g_ram[getArg(g_program[statement])]
+            g_register %= 256
+            statement++
+            continue
         }
+
         if(g_program[statement].match(/^g/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            g_register = g_ram[g_ram[num]];
-            statement++;
-            continue;
+            g_register = g_ram[g_ram[getArg(g_program[statement])]]
+            statement++
+            continue
         }
+
         if(g_program[statement].match(/^o/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            g_ram[g_ram[num]] = g_register;
-            statement++;
-            continue;
+            g_ram[g_ram[getArg(g_program[statement])]] = g_register
+            statement++
+            continue
         }
+
         if(g_program[statement].match(/^r/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            let read = 0;
-            g_ram[num] = (read = parseInt(prompt('input'))) > 255 && read < 0 ? 0 : read;
-            statement++;
-            continue;
+            let read = 0
+            g_ram[getArg(g_program[statement])] = (read = parseInt(prompt('input'))) > 255 && read < 0 ? 0 : read
+            statement++
+            continue
         }
+
         if(g_program[statement].match(/^b/)) {
-            if (g_register == 0) {
-                let num;
-                if(g_program[statement].length == 2) {
-                    num = parseInt(g_program[statement].charAt(1));
-                }
-                else if(g_program[statement].length == 3) {
-                    num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-                }
-                else {
-                    num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-                }
-                statement = g_ram[num];
-                continue;
-            } else {
-                statement++;
-                continue;
-            }
+            if (g_register == 0) 
+                statement = g_ram[getArg(g_program[statement])]
+             else 
+                statement++
+            continue    
         }
         if(g_program[statement].match(/^i/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            g_ram[num] += g_register;
-            g_ram[num] %= 256;
-            statement++;
-            continue;
+            g_ram[getArg(g_program[statement])] += g_register
+            g_ram[getArg(g_program[statement])] %= 256
+            statement++
+            continue
         }
         if(g_program[statement].match(/^t/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            alert(g_ram[num]);
-            statement++;
-            continue;
+            alert(g_ram[getArg(g_program[statement])])
+            statement++
+            continue
         }
         if(g_program[statement].match(/^s/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            g_register ^=g_ram[num];
-            statement++;
-            continue;
+            g_register ^=g_ram[getArg(g_program[statement])]
+            statement++
+            continue
         }
         if(g_program[statement].match(/^a/)) {
-            let num;
-            if(g_program[statement].length == 2) {
-                num = parseInt(g_program[statement].charAt(1));
-            }
-            else if(g_program[statement].length == 3) {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)));
-            }
-            else {
-                num = parseInt(g_program[statement].charAt(1).concat(g_program[statement].charAt(2)).concat(g_program[statement].charAt(3)));
-            }
-            g_register += g_ram[g_ram[num]];
-            g_register %= 256;
-            statement++;
-            continue;
+            g_register += g_ram[g_ram[getArg(g_program[statement])]]
+            g_register %= 256
+            statement++
+            continue
         }
     }
 }
 
-function checkSyntax(program, strict) {
+function checkSyntax(program) {
     if(program.length > 256) {
-        console.error('Program is too big >> length is', program.length);
-        return false;
+        console.error('Program is too big >> length is', program.length)
+        return false
     }
 
-    let canCompile = true;
+    let canCompile = true
     program.forEach(element => {
-        if (!isValidStatement(element, strict)) {
-            console.error(element, '<< invalid syntax');
-            canCompile = false;
+        if (!isValidStatement(element)) {
+            console.error(element, '<< invalid syntax')
+            alert(element, '<< invalid syntax')
+            canCompile = false
         }
-    });
-    if (!canCompile) return false;
-    console.log('Program is correct');
+    })
+
+    if (!canCompile) return false
+
+    console.log('Program is correct')
     return true
 }
 
-function setProgram(statement, line) {
-    if(line > 255 || line < 0) return false;
-    g_program[line] = statement;
-    return true;
+function getArg(statement) {
+    return parseInt(statement.substring(1))
 }
 
-function isValidStatement(statement, strict) {
-    if (!strict) return statement.match(/^((R|T)|([GOBISA]([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])))$/);
-    return statement.match(/^((R|T)|([GOBISAgorbitsa]([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])))$/);
+function isValidStatement(statement) {
+    return statement.match(/^((R|T)|([GOBISAgorbitsa]([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])))$/)
 }
